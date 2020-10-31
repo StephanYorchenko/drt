ad15766ded4c08895208231affc41e1476b30673
lololozhkin <alexand3r.lozhkin@gmail.com>
2020-10-31 23:43:33 +0500

    small fixies

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,12 +1,11 @@
 from flask import Blueprint
 from . import main, announcement
 
-
 Routes = Blueprint('routes', __name__,
-				   template_folder='templates')
+                   template_folder='templates')
 
 Routes.add_url_rule('/', 'main', view_func=main.main)
 Routes.add_url_rule(
-	'/page',
-	'get_count',
-	view_func=announcement.get_announcements)
+    '/announcement',
+    'get_count',
+    view_func=announcement.get_announcements)
