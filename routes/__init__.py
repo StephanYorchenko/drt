9e698d576952093a12f7d5f2925e4a8e511a464e
lololozhkin <alexand3r.lozhkin@gmail.com>
2020-10-31 21:41:25 +0500

    announcement handler added

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,7 +1,12 @@
 from flask import Blueprint
-from . import main
+from . import main, announcement
+
 
 Routes = Blueprint('routes', __name__,
 				   template_folder='templates')
 
 Routes.add_url_rule('/', 'main', view_func=main.main)
+Routes.add_url_rule(
+	'/get_count',
+	'get_count',
+	view_func=announcement.get_announcements)
