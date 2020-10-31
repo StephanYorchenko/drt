836698059fae24419699d3066cd20b3514685f8e
Yury Frolov <yura200253@gmail,com>
2020-10-31 19:20:14 +0500

    Added Hostess' methods & fixed User

--- Domain/Users/Hostess.py
+++ Domain/Users/Hostess.py
@@ -1,1 +1,17 @@
-pass
+from Domain.User import User
+from Domain.Roles import Role
+
+
+class Hostess(User):
+    def __init__(self, user_id: int, username: str, password):
+        super().__init__(user_id, username, password)
+        self.role = Role.HOSTESS
+
+    def get_requests(self):
+        pass
+
+    def accept_request(self, request):
+        pass
+
+    def deny_request(self, request):
+        pass
