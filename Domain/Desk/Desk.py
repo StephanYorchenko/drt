2e7e5d26fdfa66dadef93e1d844c10f370d6889e
Yury Frolov <yura200253@gmail,com>
2020-10-31 20:21:29 +0500

    Added Desk.get_announcements

--- Domain/Desk/Desk.py
+++ Domain/Desk/Desk.py
@@ -1,13 +1,17 @@
 import typing
+
 from Domain.Desk.Announcement import Announcement
+from infrastructure.DBAnnouncement import DBAnnouncement
 
 
 class Desk:
     def __init__(self):
         pass
 
-    def get_announcements(self) -> typing.List[Announcement]:
-        pass
+    @staticmethod
+    def get_announcements(n) -> typing.List[Announcement]:
+        jsons = DBAnnouncement.get_announcements()[:n]
+        return list(map(lambda data: Announcement.from_json(data), jsons))
 
     def add_announcement(self):
         pass
