8487424613a1dc3f100db7d967b903ef420d1454
Yury Frolov <yura200253@gmail,com>
2020-11-01 01:50:22 +0500

    Fixed fucking bugs 5

--- domain/announcement/announcement.py
+++ domain/announcement/announcement.py
@@ -5,7 +5,7 @@ from infrastructure.serializable import Serializable
 
 class Announcement(Serializable):
     def __init__(self, title: str, text: str, user_id: int):
-        self.topic = title
+        self.title = title
         self.text = text
         self.user_id = user_id
 
