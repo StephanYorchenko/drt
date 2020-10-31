2aebbd813f63ce1f3f29d1309be1130e6e52a1e0
Anchutin Nikita <asd200101@ya.ru>
2020-11-01 00:43:07 +0500

    fix fucking bugs

--- routes/announcement.py
+++ routes/announcement.py
@@ -2,21 +2,21 @@ from flask import request, render_template, jsonify
 
 
 def render_page():
-	return render_template('announcement.html', is_admin=True, is_hostes=True)
+    return render_template('announcement.html', is_admin=True, is_hostes=True)
 
 
 def get_announcements():
-	page_number = request.args.get('page_number')
-	print(page_number)
-	return jsonify({
-			'count': 15,
-			'announcements':
-				[
-						{'title': 'Hello',
-						 'text': 'World',
-						 'date': '2020-12-20'}
-				] * 10
-	})
+    page_number = request.args.get('page_number')
+    print(page_number)
+    return jsonify({
+        'count': 15,
+        'announcements':
+            [
+                {'title': 'Hello',
+                 'text': 'World',
+                 'date': '2020-12-20'}
+            ] * 10
+    })
 
 # from domain.desk.announcement_desk import AnnouncementDesk
 # from .get_items_from_desk import jsonify_n_serialized_objects_from_desk
--- routes/requests.py
+++ routes/requests.py
@@ -1,6 +1,6 @@
 from .get_items_from_desk import jsonify_n_serialized_objects_from_desk
-from domain.desk.desk import Desk
+from domain.announcement.announcement_desk import AnnouncementDesk
 
 
 def get_announcements():
-    return jsonify_n_serialized_objects_from_desk(Desk,  'requests')
+    return jsonify_n_serialized_objects_from_desk(AnnouncementDesk,  'requests')
