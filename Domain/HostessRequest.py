0b22b8207816b30e34f76cc4ff120b8e67bd4a1a
lololozhkin <alexand3r.lozhkin@gmail.com>
2020-10-31 17:36:49 +0500

    some props are added to hostess requests

--- Domain/HostessRequest.py
+++ Domain/HostessRequest.py
@@ -1,3 +1,24 @@
+import typing
+from .HostessRequestType import HostessRequestType
+
+
 class Request:
-    def __init__(self):
-        pass
+    def __init__(self,
+                 request_type: HostessRequestType,
+                 comment: str,
+                 request_id: int):
+        self._type = request_type
+        self._comment = comment
+        self._id = request_id
+
+    @property
+    def request_id(self):
+        return self._id
+
+    @property
+    def comment(self):
+        return self._comment
+
+    @property
+    def request_type(self):
+        return self._type
