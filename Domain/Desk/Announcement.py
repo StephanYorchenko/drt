906e1a8a1b943a9e99826e8b27860db7e1502527
Yury Frolov <yura200253@gmail,com>
2020-10-31 20:49:18 +0500

    Small fixes

--- Domain/Desk/Announcement.py
+++ Domain/Desk/Announcement.py
@@ -2,7 +2,8 @@ from Domain.Serializable import Serializable
 
 
 class Announcement(Serializable):
-    def __init__(self, ann_id: int, topic: str, text: str):
+    def __init__(self, ann_id: int, topic: str, text: str, user_id: int):
         self.id = ann_id
         self.topic = topic
         self.text = text
+        self.user_id = user_id
