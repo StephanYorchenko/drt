7ba9529651a925e94687a0d650f57001713103a3
Anchutin Nikita <asd200101@ya.ru>
2020-10-31 20:12:53 +0500

    small fixes

--- infrastructure/DBAnnouncement.py
+++ infrastructure/DBAnnouncement.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, Text, ForeignKey, String
+from sqlalchemy import Column, Integer, Text, ForeignKey, String, DateTime
 from infrastructure import Base
+from api import dbconn
 
 
 class DBAnnouncement(Base):
@@ -8,4 +9,14 @@ class DBAnnouncement(Base):
     id = Column(Integer, primary_key=True)
     text = Column(Text, nullable=True)
     topic = Column(String, nullable=True)
+
     user_id = Column(Integer, ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+
+    # @staticmethod
+    # def check_user(i_username, i_password) -> bool:
+    #     user = DBAnnouncement.get_user(name=i_username)
+    #     return bool(user) and user.password == i_password
+
+    @staticmethod
+    def get_announcements():
+        return "lol"
