c9845d31b13a3853c350c9e0cba2da2de0f6da9e
Yury Frolov <yura200253@gmail,com>
2020-10-31 19:54:03 +0500

    Small fix

--- Domain/Users/Hostess.py
+++ Domain/Users/Hostess.py
@@ -1,12 +1,15 @@
+import typing
+
 from .User import User
 from .Roles import Role
+from Domain.Request import HostessRequest
 
 
 class Hostess(User):
     def __init__(self, user_id: int, username: str, password):
         super().__init__(user_id, username, password, Role.HOSTESS)
 
-    def get_requests(self):
+    def get_requests(self) -> typing.List[HostessRequest]:
         pass
 
     def accept_request(self, request):
