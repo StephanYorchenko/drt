25c8be612bc6d5d0271cd34e58fbba3c2a416c38
Yury Frolov <yura200253@gmail,com>
2020-10-31 18:09:12 +0500

    Added serialization to User

--- Domain/User.py
+++ Domain/User.py
@@ -1,21 +1,39 @@
 import typing
+import json
 from .Roles import Role
 from .HostessRequest import Request
 from .Table import Table
 
 
 class User:
-    def __init__(self, user_id: int, role: Role, login: str, password: str):
+    def __init__(self, user_id: int, role: Role, username: str, password):
         self.id = user_id
         self.role = role
-        self.login = login
-        self.set_password(password)
+        self.username = username
+        self.password = password
 
-    def set_password(self, password: str):
-        pass
+    def set_username(self, username: str):
+        self.username = username
+
+    def set_password(self, password):
+        self.password = password
 
     def make_request(self, topic: str, description: str):
         pass
 
     def try_book_table(self, table: Table):
         pass
+
+    def to_json(self):
+        return bytes(json.dumps({'id': self.id,
+                                'role': self.role,
+                                 'username': self.username,
+                                 'password': self.password}))
+
+    @staticmethod
+    def from_json(data: bytes):
+        dictionary = json.loads(data)
+        return User(dictionary['id'],
+                    dictionary['role'],
+                    dictionary['username'],
+                    dictionary['password'])
