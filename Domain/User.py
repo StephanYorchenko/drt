083212da18a151a27e32387ca0aa6e190cd5bb8d
Yury Frolov <yura200253@gmail,com>
2020-10-31 17:54:21 +0500

    Added Desk & fixes

--- Domain/User.py
+++ Domain/User.py
@@ -5,9 +5,14 @@ from .Table import Table
 
 
 class User:
-    def __init__(self, user_id: int, role: Role):
+    def __init__(self, user_id: int, role: Role, login: str, password: str):
         self.id = user_id
         self.role = role
+        self.login = login
+        self.set_password(password)
+
+    def set_password(self, password: str):
+        pass
 
     def make_request(self, topic: str, description: str):
         pass
