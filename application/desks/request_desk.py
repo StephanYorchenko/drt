9cf531072a9c3267a6a21d1f7a6a169108699979
Yury Frolov <yura200253@gmail.com>
2020-11-27 09:46:10 +0500

    Serializers are delegating now & small refactor 5

--- application/desks/request_desk.py
+++ application/desks/request_desk.py
@@ -1,13 +1,10 @@
 from application.desks.desk import Desk
-from domain.request.request import Request
-from infrastructure.db_classes.db_request import DBRequest
 from application.domain_serializers.request_serializer import RequestSerializer
-
+from infrastructure.db_classes.db_request import DBRequest
 
 REQUEST_COUNT = 10
 
 
 class RequestDesk(Desk):
     def __init__(self):
-        super().__init__(Request, DBRequest, RequestSerializer(),
-                         REQUEST_COUNT)
+        super().__init__(DBRequest, RequestSerializer(), REQUEST_COUNT)
--- application/domain_serializers/request_serializer.py
+++ application/domain_serializers/request_serializer.py
@@ -4,14 +4,19 @@ from domain.request.request import Request
 from domain.request.requestType import HostessRequestType
 
 
-class RequestSerializer(DomainSerializer):
-    def to_json(self, request: Request) -> Dict[str, str]:
-        # noinspection PyProtectedMember
-        return {'id': request._id, 'request_type': request._type,
-                'comment': request._comment, 'user_id': request._user}
+def to_json(request: Request) -> Dict[str, str]:
+    # noinspection PyProtectedMember
+    return {'id': request._id, 'request_type': request._type,
+            'comment': request._comment, 'user_id': request._user}
 
-    def from_json(self, data: Dict[str, str]) -> Request:
-        return Request(int(data['id']),
-                       HostessRequestType(data['request_type']),
-                       data['comment'],
-                       int(data['user_id']))
+
+def from_json(data: Dict[str, str]) -> Request:
+    return Request(int(data['id']),
+                   HostessRequestType(data['request_type']),
+                   data['comment'],
+                   int(data['user_id']))
+
+
+class RequestSerializer(DomainSerializer):
+    def __init__(self):
+        super().__init__(from_json, to_json)
