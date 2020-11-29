0661b11a40c3e7a85f8df1ceea55108fe995b2cd
Stepan Yurchenko <stepan33314@yandex.ru>
2020-11-29 22:36:38 +0500

    Fix

--- ui/routes/auth.py
+++ ui/routes/auth.py
@@ -8,24 +8,17 @@ def check_auth():
     user_hash = request.cookies.get('user_hash')
 
     result = DBUser.check_user(username, user_hash)
-    us = DBUser.get_user(name=username)
-    print('db:', us.name, us.user_hash)
-    print(username, user_hash, result)
-    DBUser.update_user_hash(username, 'kek')
-
     return jsonify({"result": result})
 
 
 def try_authorize():
     username = request.form.get('name')
     password = request.form.get('password')
-    print(username, password)
 
     user = DBUser.get_user(name=username)
     name = user.name if user is not None else ''
     role = str(user.role.value) if user is not None else ''
 
-    print(user.password)
     resp = make_response(
         jsonify({
             "authorized": bool(user) and user.password == password,
