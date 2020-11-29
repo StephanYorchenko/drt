8f67b1c4120f7e4c268624433554333af37663cf
Stepan Yurchenko <stepan33314@yandex.ru>
2020-11-29 23:19:53 +0500

    Add src

--- ui/routes/auth.py
+++ ui/routes/auth.py
@@ -1,40 +1,21 @@
-from flask import jsonify, make_response, redirect, request
-from infrastructure import DBUser
+from flask import jsonify, make_response, redirect
 
 
 def check_auth():
-    username = request.args.get('name')
-    user_hash = request.args.get('password')
-
-    result = DBUser.check_user(username, user_hash)
-
-    return jsonify({"result": result})
+	return jsonify({"result": True})
 
 
 def try_authorize():
-    username = request.form.get('name')
-    user_hash = request.form.get('password')
-
-    user = DBUser.get_user(name=username)
-    name = user.name if user is not None else ''
-    role = str(user.role.value) if user is not None else ''
-
-    resp = make_response(
-        jsonify({
-            "authorized": bool(user) and user.user_hash == user_hash,
-            'Name': name,
-            'Role': role
-        })
-    )
-
-    resp.set_cookie('name', name)
-    resp.set_cookie('role', role)
-
-    return resp
+	resp = make_response(jsonify({"authorized": True,
+								  'Name': 'Stephan',
+								  'Role': 1}))
+	resp.set_cookie('name', 'Stephan')
+	resp.set_cookie('role', '1')
+	return resp
 
 
 def logout():
-    resp = make_response(redirect('/'))
-    resp.set_cookie('name', '', expires=0)
-    resp.set_cookie('role', '', expires=0)
-    return resp
+	resp = make_response(redirect('/'))
+	resp.set_cookie('name', '', expires=0)
+	resp.set_cookie('role', '', expires=0)
+	return resp
