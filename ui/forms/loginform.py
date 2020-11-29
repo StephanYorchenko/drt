acb05e06f2e703b0a542f309a9d2ae157fbe1051
Stepan Yurchenko <stepan33314@yandex.ru>
2020-11-29 11:01:34 +0500

    Remove loginmanager in python and forms module

--- ui/forms/loginform.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class LoginForm(FlaskForm):
-    username = StringField('Имя пользователя', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    submit = SubmitField('Вход')
