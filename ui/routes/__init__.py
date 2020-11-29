0d3a320aefe2ba6475989105520369a1dca5db88
Stepan Yurchenko <stepan33314@yandex.ru>
2020-11-29 22:54:46 +0500

    add

--- ui/routes/__init__.py
+++ ui/routes/__init__.py
@@ -1,39 +1,44 @@
-from flask import Blueprint
+from flask import Blueprint, send_from_directory
 from . import main, announcement, requests, auth
 
 Routes = Blueprint('routes', __name__, template_folder='templates')
 
 Routes.add_url_rule(
-    '/',
-    'announcements',
-    view_func=announcement.render_page,
-    methods=["GET"]
+		'/',
+		'main',
+		view_func=lambda: send_from_directory('static/dist', 'index.html')
 )
 
 Routes.add_url_rule(
-    '/get_count',
-    'get_count',
-    view_func=announcement.get_announcements,
-    methods=["GET"],
+		'/dist/<path:path>',
+		'static',
+		view_func=lambda path: send_from_directory('static/dist', path)
 )
 
 Routes.add_url_rule(
-    '/auth',
-    'auth',
-    view_func=auth.try_authorize,
-    methods=["POST"],
+		'/api/get_count',
+		'get_count',
+		view_func=announcement.get_announcements,
+		methods=["GET"],
 )
 
 Routes.add_url_rule(
-    '/check',
-    'check',
-    view_func=auth.check_auth,
-    methods=["POST"],
+		'/api/auth',
+		'auth',
+		view_func=auth.try_authorize,
+		methods=["POST"],
 )
 
 Routes.add_url_rule(
-    '/logout',
-    'logout',
-    view_func=auth.logout,
-    methods=["GET"],
+		'/api/check',
+		'check',
+		view_func=auth.check_auth,
+		methods=["POST"],
+)
+
+Routes.add_url_rule(
+		'/api/logout',
+		'logout',
+		view_func=auth.logout,
+		methods=["GET"],
 )
