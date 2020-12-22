bcfbeddaac4cf7e644717a5ba4452f4ecca96ceb
lololozhkin <alexand3r.lozhkin@gmail.com>
2020-12-22 13:39:40 +0500

    aaaa

--- infrastructure/database_manager/dblink.py
+++ infrastructure/database_manager/dblink.py
@@ -12,11 +12,3 @@ class DBConn:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.db.close()
-
-
-class Db(object):
-    def __init__(self, test=False):
-        self.test = test
-        self.engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
-        self.bdsession = sessionmaker(bind=self.engine, autocommit=True)()
-        self.connect = self.engine.connect()
