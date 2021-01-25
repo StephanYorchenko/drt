5a55015713a32c816d3e3e0c61af9fa026fe0e2a
Stepan Yurchenko <stepan33314@yandex.ru>
2021-01-25 15:28:55 +0500

    test template

---
+++ unit_tests/routes_tests.py
@@ -0,0 +1,35 @@
+import unittest
+from unittest.mock import patch
+
+import sqlalchemy
+
+from start import get_app
+
+
+class RoutesTest(unittest.TestCase):
+	def setUp(self) -> None:
+		self.app = get_app()
+
+	def test_default_rendering(self):
+		with self.app.test_client() as c:
+			with c.get("/") as rv:
+				self.assertTrue(rv)
+
+	def test_render_login_on_noauth(self):
+		with self.app.test_client() as c:
+			with c.get("/login") as login_page:
+				l_page_content = login_page.data
+			with c.get('/request') as request_page:
+				r_page_contet = request_page.data
+
+		self.assertEqual(l_page_content, r_page_contet)
+
+	@patch.object(sqlalchemy, 'create_engine')
+	def test_api_request_without_auth(self, mock_sql):
+		with self.app.test_client() as c:
+			with c.get("/api/request") as api_req:
+				self.assertTrue(True)
+
+
+if __name__ == '__main__':
+	unittest.main()
