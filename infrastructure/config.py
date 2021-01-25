53fcdd96cf9d8f76109c49e619b891055f110144
Stepan Yurchenko <stepan33314@yandex.ru>
2021-01-25 17:51:26 +0500

    Add db link in Config

--- infrastructure/config.py
+++ infrastructure/config.py
@@ -3,5 +3,5 @@ import os
 
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'send_goge'
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "sqlite:///infrastucture/database.db"
 	SQLALCHEMY_TRAK_MODIFICATIONS = False
